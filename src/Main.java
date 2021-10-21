dc68d454a578950dcca5e101af96b2379ea4ba8b
cristianbrb <cristian.brebeanu@yahoo.com>
2021-10-21 21:24:40 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+System.out.println("Hello World!");
+short myShort = 2;
+ short mySecondShort;
+
+mySecondShort = 2;
+int myInt = mySecondShort + 2;
+System.out.println(myInt);
+
+long mySecondInt = 123456L;
+long result = myInt + 12345698l;
+float myFloat = 3.3f;
+double myDouble = myFloat = myInt;
+
+    }
+
+
+}
