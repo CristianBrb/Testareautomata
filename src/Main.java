13c36d7dd6e6411254392718d3e901b6359dcbdd
cristianbrb <cristian.brebeanu@yahoo.com>
2021-10-23 12:38:32 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 System.out.println("Hello World!");
-short myShort = 2;
+/*short myShort = 2;
  short mySecondShort;
 
 mySecondShort = 2;
@@ -14,9 +14,27 @@ long result = myInt + 12345698l;
 float myFloat = 3.3f;
 double myDouble = myFloat = myInt;
 byte age = 3
-
-
  */
+
+
+     System.out.println(3+2);
+     float mysecondFloat = 10/3F;
+     System.out.println (mysecondFloat);
+
+     
+        System.out.println(2+2+ 21-2*(2+2));
+
+        System.out.println(-5+8*6);
+        System.out.println((55+9) % 9 );
+        System.out.println(20+ -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3 );
+
+
+
+
+
+
+
     }
 
 
